0689f32b23a6ff09a7309ed10bc4855ead0deb6f
Kleber Marques <116022261+300524@users.noreply.github.com>
2025-06-03 18:25:54 -0300

    primeiro exercicio
    Neste primeiro exercio temos os tipos de variaveis como : int, float, boolean, date.

---
+++ exercicio.py
@@ -0,0 +1,21 @@
+'''
+Será realizado codigos basicos, para 
+verificar se foi aprendido sobre variavél
+
+'''
+
+nome = 'Kleber'
+sobrenome = 'Marques'
+idade = 37
+ano__nascimento = 1988
+maior_de_idade = (idade >= 18)
+altura_metros = 1.75
+
+
+
+print("Seu nome é:", nome)
+print("Seu sobrenome é:", sobrenome)
+print("Sua idade:", idade)
+print("O ano do seu nascimento:", ano__nascimento)
+print("Você é maior de idade:", maior_de_idade)
+print("Sua altura:", altura_metros, "metros")
